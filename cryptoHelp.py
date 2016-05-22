f023544a1a20486fcc415bb9f89b2f58ebc34184
Alan Sayler <AlanJSayler@gmail.com>
2016-05-21 22:13:22 -0500

    Added decryption of Affine ciphers.

--- cryptoHelp.py
+++ cryptoHelp.py
@@ -23,19 +23,32 @@ def isPositiveInt(a):
     if a % 1 != 0 or a < 1:
         return 0
     return 1
+
+#Since the alphabet is limited to ASCII characters, it can never
+#be that big, and hence, inverting a key will not take long by brute
+#force. We could change this to Euclid's extended algorithm for a
+#lower time complexity
+def inverseInAlphabet(a):
+    for i in range(0,len(alphabet)):
+        if (a * i) % len(alphabet) == 1:
+            return i
+    return -1
+
+def validateAffineKeys(coefKey,offKey):
+    if coefKey == 1:
+        print("Warning: A value of 1 as the coefficient key reduces" +
+             " the affine cipher to being a Caesar shit")
+    if isPositiveInt(offKey) != 1 or isPositiveInt(coefKey) != 1:
+        print("Error: You need to choose positive integers for your keys")
+        return -1
+    if gcd(coefKey, len(alphabet)) != 1:
+        print("Error: you need to choose positive integers for your keys")
+        return -1
+    return 1
 #encrypt an affine cipher given an a,b key pair
 def encryptAffine(coefKey, offKey, plainText):
-    if coefKey == 1:
-        print("Warning: A value of 1 as the coefficient keyi " +
-              "reduces the affine cipher to being a Caesar shift")
-    if isPositiveInt(offKey) != 1 or isPositiveInt(coefKey) != 1:
-        print("Error: you need to choose positive integers for you keys") 
-        return -1
-    #Check if the coefficient key is coprime to the length of the Alphabet
-    if gcd(coefKey, len(alphabet)) != 1:
-        print("Error: you need to pick a coefficient key that is coprime" +
-              " to the length of your alphabet")
-        return -1
+    if validateAffineKeys(coefKey,offKey) != 1:
+        return
     cipherText = ''
     for i in range(0,len(plainText)):
         if(letterToDigit(plainText[i]) == -1):
@@ -43,3 +56,15 @@ def encryptAffine(coefKey, offKey, plainText):
             return
         cipherText += alphabet[(letterToDigit(plainText[i]) * coefKey + offKey) % len(alphabet)]
     return cipherText
+
+#inverse function of an affine cipher given a,b key pair
+def decryptAffine(coefKey, offKey, cipherText):
+    if validateAffineKeys(coefKey,offKey) != 1:
+        return
+    plainText = ''
+    for i in range(0,len(cipherText)):
+        if(letterToDigit(cipherText[i]) == -1):
+            print("Error: Cannot encrypt a letter not in the specified alphabet!")
+            return
+        plainText += alphabet[((letterToDigit(cipherText[i]) - offKey) * inverseInAlphabet(coefKey)) %len(alphabet)]
+    return plainText
